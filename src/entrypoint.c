aa111a684bf5b81e9cbeb2cc149311e5f220c606
andy-thomason <andy@andythomason.com>
2020-12-19 13:59:43 +0000

    Simplyfy entrypoint.c

--- src/entrypoint.c
+++ src/entrypoint.c
@@ -1,18 +1,6 @@
-/*
-// Take the address of the wrap__hello stub function to avoid
-// the linker removing the static library.
-//
-// This will be removed in future versions with the module macro.
-void wrap__hello();
+struct DllInfo;
 
-void *x = (void*)&wrap__hello;
-*/
+void R_init_helloextendr(struct DllInfo *dll);
 
-
-#define R_NO_REMAP
-#define STRICT_R_HEADERS
-#include <Rinternals.h>
-
-void R_init_helloextendr(DllInfo *dll);
-
+// Ensure that the static library is retained by th elinker.
 void *p = (void*)&R_init_helloextendr;
