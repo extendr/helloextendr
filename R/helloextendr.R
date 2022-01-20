a687f1ad77c2c3a33c9310384d5749684a55f854
Ilia <ilia.kosenkov.at.gm@gmail.com>
2022-01-20 07:37:51 +0300

    Updating project template (#13)
    * Modified Makevars, updated wrappers
    
    * Updated docs
    
    * Updating CI
    
    * Simplifying CI
    
    * Reordered steps
    
    * Removed debug trigger

--- R/helloextendr.R
+++
@@ -1,7 +0,0 @@
-#' An example package demonstrating the use of the extendr crate from R.
-#'
-#' An example package demonstrating the use of the extendr crate from R.
-#' @name helloextendr
-#' @docType package
-#' @useDynLib helloextendr, .registration = TRUE
-NULL
